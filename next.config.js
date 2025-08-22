4a278868f718334bf6c057b99b9a845fcb5b3129
unknown <dylanadekpe14@gmail.com>
2025-08-22 11:50:21 +0000

    github actions config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     domains: ['dl.airtable.com'],
+    unoptimized: true,
   },
   env: {
     NEXT_PUBLIC_WHATSAPP_NUMBER: process.env.WHATSAPP_NUMBER,
